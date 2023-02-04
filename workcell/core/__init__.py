4af8f01beba35bb8f50de7a87e9faffd63ad49fc
jiandong <jiandongj@qq.com>
2023-02-04 15:53:28 +0800

    remove useless import;

--- workcell/core/__init__.py
+++ workcell/core/__init__.py
@@ -1,7 +1,2 @@
-import workcell
-
-# define the version before the other imports since these need it
-__version__ = workcell.__version__
-
-from .model import Workcell, get_callable, get_spec
-from .utils import name_to_title, format_workcell_entrypoint
+from .model import Workcell
+from .components import Component
