ffe5b8c2469abfc1989d6ac08b7527854069a320
jiandong <jiandongj@qq.com>
2023-02-04 15:57:42 +0800

    for collabration;

--- workcell/__init__.py
+++ workcell/__init__.py
@@ -1,5 +1,3 @@
 """Information about this library. This file will automatically changed (by poetry-bump-version)."""
 
 __version__ = "0.0.21"
-__author__ = "jiandong"
-__email__ = "jiandong@weanalyze.co"
