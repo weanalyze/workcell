b37796df2cecdf64c2cf721e5e175740a53befd5
jiandong <jiandongj@qq.com>
2023-02-08 19:06:41 +0800

    update workcell ui version;

--- workcell/__init__.py
+++ workcell/__init__.py
@@ -1,6 +1,6 @@
 """Information about this library. This file will automatically changed (by poetry-bump-version)."""
 
 __version__ = "0.0.21"
-__ui_version__ = "0.1.3"
+__ui_version__ = "0.1.6"
 
 from .core import Workcell
