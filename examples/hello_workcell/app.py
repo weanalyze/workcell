bb59d2210bd0a4684b62a5710ecd5be2cadda904
jiandong <jiandongj@qq.com>
2023-02-12 17:26:35 +0800

    update examples;

--- examples/hello_workcell/app.py
+++ examples/hello_workcell/app.py
@@ -1,8 +1,9 @@
 from pydantic import BaseModel
-
+import workcell
 
 class Input(BaseModel):
     message: str
+
 
 class Output(BaseModel):
     message: str
@@ -11,3 +12,6 @@ class Output(BaseModel):
 def hello_workcell(input: Input) -> Output:
     """Returns the `message` of the input data."""
     return Output(message=input.message)
+
+
+app = workcell.create_app(hello_workcell)
