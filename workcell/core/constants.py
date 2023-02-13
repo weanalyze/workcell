efb8fb103b01963c91a8abe824aa855a45dc2bf0
jiandong <jiandongj@qq.com>
2023-02-14 00:27:21 +0800

    refactor ui manifest to support hot-fix;

--- workcell/core/constants.py
+++ workcell/core/constants.py
@@ -6,11 +6,10 @@ TEMPLATE_FOLDER=os.path.join(WORKCELL_FOLDER, "templates")
 SCAFFOLD_FOLDER=os.path.join(TEMPLATE_FOLDER, "scaffold")
 RUNTIME_FOLDER=os.path.join(TEMPLATE_FOLDER, "runtime")
 # Default frontend template
+WORKCELL_UI_MANIFEST = "https://api.weanalyze.co/storage/v1/object/public/manifests/workcell_ui_manifest.json"
 WORKCELL_UI_TEMPLATE = os.path.join(TEMPLATE_FOLDER, "frontend", "index.html")
 
 # Default workcell custom domain
-WORKCELL_API_GATEWAY="https://fun.weanalyze.co/api/v1"
-WORKCELL_API_SERVER="https://workcell.live/api/v1"
 WORKCELL_SERVER_NAME="127.0.0.1"
 WORKCELL_SERVER_PORT=7860
 WORKCELL_NUM_PORTS=100
