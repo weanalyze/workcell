32d2e1cce914eb6765f26d70348a5a8d5c17bf2a
jiandong <jiandongj@qq.com>
2023-02-14 21:03:53 +0800

    add support for universal `workcell_id`;

--- workcell/core/constants.py
+++ workcell/core/constants.py
@@ -9,6 +9,10 @@ RUNTIME_FOLDER=os.path.join(TEMPLATE_FOLDER, "runtime")
 WORKCELL_UI_MANIFEST = "https://api.weanalyze.co/storage/v1/object/public/manifests/workcell_ui_manifest.json"
 WORKCELL_UI_TEMPLATE = os.path.join(TEMPLATE_FOLDER, "frontend", "index.html")
 
+# Supported Providers
+SUPPORT_PROVIDER = ["huggingface", "weanalyze"]
+SUPPORT_RUNTIME = ["python3.8", "python3.9"]
+
 # Default workcell custom domain
 WORKCELL_SERVER_NAME="127.0.0.1"
 WORKCELL_SERVER_PORT=7860
