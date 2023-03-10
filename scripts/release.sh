539623bc8e4f26eb34c027b897726888aa63c201
jiandong <jiandongj@qq.com>
2023-03-10 10:01:35 +0800

    remove scripts;

--- scripts/release.sh
+++
@@ -1,6 +0,0 @@
-# update version in pyproject.toml
-poetry version patch
-# tag and push by github actions
-git checkout main
-git tag $(poetry version -s)
-git push --tags origin main $(poetry version -s)
