f9a428409fef9a04f4be110d8ee7c2df3fcf00e9
jiandong <jiandongj@qq.com>
2023-03-12 00:20:58 +0800

    minor version patch;

--- workcell/__init__.py
+++ workcell/__init__.py
@@ -1,6 +1,6 @@
 """Information about this library. This file will automatically changed (by poetry-bump-version)."""
 
-__version__ = "0.0.30"
+__version__ = "0.0.31"
 __ui_version__ = "0.1.6"
 
 from .core import Workcell
